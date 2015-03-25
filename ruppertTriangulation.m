f557197e8f45e0ab14c701fd833ba31e2d6ad47c
Kristian Hartikainen <kristian@statwing.com>
2015-03-25 23:08:02 +0200

    Added the "while any segment s is encroached upon" part to the algorithm. Something seems to be wrong, but the high level stuff should be correct

--- ruppertTriangulation.m
+++ ruppertTriangulation.m
@@ -6,9 +6,40 @@ function [ T ] = ruppertTriangulation( points, segments, threshold )
 % argument 3 for the square side
 [X, Y, S] = squareBound(points.x, points.y, segments, 3);
 
-DT = delaunayTriangulation([X Y]);
 
-enroached = true;
+V = [X;Y];
+DT = delaunayTriangulation(X', Y');
+
+encroached = true;
+while (encroached)
+    i=1;
+    encroached_upon_s = true;
+    while (encroached_upon_s)
+        s = S(:, i);
+        end_pts = V(:, s);
+        d = pdist(end_pts);
+        mid_pt = sum(end_pts, 2)/2; % mid = [(x1 + x2)/2, (y1 + y2)/2]
+        if any(pdist2([X;Y]', mid_pt') < d/2)
+            % splitSeg
+            % Add midpoint to V
+            V = [V mid_pt];
+            % Remove s from S
+            S(:, i) = [];
+            % Add the two halves s1 and s2 to S.
+            new_pt_idx = size(V, 2);
+            s1 = [s(1); new_pt_idx];
+            s2 = [s(2); new_pt_idx];
+            % [s';new_pt_idx new_pt_idx]
+            S(:, end+1:end+2) = [s1 s2];
+        end
+        i = i+1
+        S
+        if (i>size(S, 2)) encroached_upon_s = false; end
+        pause;
+    end
+    encroached = false;
+end
+
 
 
 
