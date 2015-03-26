14a8bd6df9b517b555773b80ad9d86c31c3aa9fa
Risto Vuorio <risto@apportable.com>
2015-03-26 15:25:32 +0200

    pslg2: improved the point input, changed in to function

--- pslg2.m
+++ pslg2.m
@@ -1,32 +1,37 @@
 % c = 99
 % x = 120
 
-close all;
-clear all;
+function [X,Y,S] = pslg2(filename)
+    figure;
+    axis equal;
+    hold on;
 
-figure;
-axis equal;
-hold on;
-% img = imread('scrshot.png');
-img = imread('example.png');
-image(img);
-[X,Y,BUTTON] = ginput;
-segments = [];
-last_button = -123;
-len = size(X,1);
-finalpoints = [X Y];
+    img = imread(filename);
+    imagesc(img);
+    X=[];
+    Y=[];
+    [xx,yy,button] = ginput(1);
+    while (button ~= 120)
+        X = [X xx];
+        Y = [Y yy];
+        plot(xx,yy,'c*');
+        [xx,yy,button] = ginput(1);
+    end
+    segments = [];
+    finalpoints = [X' Y'];
 
-plot(X,Y,'o');
-
-[x,y,button] = ginput(2);
-while (button ~= 120)
-    apoint = [x(1) y(1)];
-    bpoint = [x(2) y(2)];
-    closestpointa = dsearchn(finalpoints,apoint);
-    closestpointb = dsearchn(finalpoints,bpoint);
-    segments = [segments [closestpointa closestpointb]'];
-    finalpointx = [finalpoints(closestpointa,1) finalpoints(closestpointb,1)];
-    finalpointy = [finalpoints(closestpointa,2) finalpoints(closestpointb,2)];
-    plot(finalpointx',finalpointy');
     [x,y,button] = ginput(2);
+    while (button ~= 120)
+        apoint = [x(1) y(1)];
+        bpoint = [x(2) y(2)];
+        closestpointa = dsearchn(finalpoints,apoint);
+        closestpointb = dsearchn(finalpoints,bpoint);
+        segments = [segments [closestpointa closestpointb]'];
+        finalpointx = [finalpoints(closestpointa,1) finalpoints(closestpointb,1)];
+        finalpointy = [finalpoints(closestpointa,2) finalpoints(closestpointb,2)];
+        plot(finalpointx',finalpointy','r','LineWidth',2);
+        [x,y,button] = ginput(2);
+    end
+
+    S = segments;
 end
